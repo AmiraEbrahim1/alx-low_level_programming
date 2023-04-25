b05164ca5ea9f0a0b5ed76c6c733207aad87a94c
AmiraEbrahim1 <amiraebrahim720@gmail.com>
2023-04-25 11:16:26 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - utilizes _putchar to print
+ * 10times the alphabet
+*/
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int d = 0;
+
+	while (d < 10)
+	{
+		while (ch < 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	_putchar('\n');
+	d++;
+	}
+}
