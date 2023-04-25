9bfa35f829c4f598d115693ea7fd4571e8e981b2
AmiraEbrahim1 <amiraebrahim720@gmail.com>
2023-04-25 16:33:10 -0700

    a

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,12 +7,10 @@
 */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while (n < 98)
 	{
-		_putchar(n + '0');
-		_putchar('\n');
-		_putchar(',');
-		_putchar(' ');
+		printf("%d, ", n);
 		n++;
 	}
+	printf("\n");
 }
