23a06f69859106b33353f64b551cb7258f569db5
AmiraEbrahim1 <amiraebrahim720@gmail.com>
2023-04-25 10:18:55 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: function prints the alpabet in lowecase.
+ *
+ * Return: 0 (Success)
+*/
+void print_alphabet(void);
+void print_alphabet(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar('
+	}
+};
+int main(void)
+{
+
+}
