d22d0c100639734d9f01068521473d23cd128c89
AmiraEbrahim1 <amiraebrahim720@gmail.com>
2023-04-26 11:16:19 -0700

    a

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print sum of even valued
+ * fibonacci sequence not exceed 4million
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	unsigned long fib1 = 0,fib2 = 1,sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fib1 +fib2;
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			total_sum += sum;
+		fib1 = fib2;
+	}
+}
