116e8a4890607a4c92388ccbb3649df0af603703
AmiraEbrahim1 <amiraebrahim720@gmail.com>
2023-04-26 12:28:45 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -35,12 +35,25 @@ int main(void)
 	{
 		if (f1o > 0)
 			printf("%d", 0);
-		initial0s--;
-	}
+		initial0s = numLength(mx) - 1 - numLength(f1);
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			initial0s--;
+		}
 
 	printf("%lu", f1);
 
 	sum = (f1 + f2) % mx;
 	sumo = f1o + f2o + (f1 + f2) / mx;
-	return (0);
+	f1 = f2;
+	f1o = f2o;
+	f2 = sum;
+	f2o = sumo;
+
+	if (count != 98)
+		printf("' ");
+	else
+		printf("\n");
+	}
 }
