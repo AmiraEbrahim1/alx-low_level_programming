424f736f9e9e2ba96f83a5ffb96c439c2b1cd7cf
AmiraEbrahim1 <amiraebrahim720@gmail.com>
2023-04-28 15:21:59 -0700

    a

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -48,3 +48,14 @@ void largest_prime_factor(long int num)
 		largest = num;
 	printf("%d\n", largest);
 }
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	largest_prime_factor(612852475143);
+
+	return (0);
+}
