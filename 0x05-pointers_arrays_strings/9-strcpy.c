d02338a583f7d860ef41290fc08ac7e71c01c110
AmiraEbrahim1 <amiraebrahim720@gmail.com>
2023-05-02 10:02:46 -0700

    a

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- *_strcpy - function copies the string pointed by
+ * *_strcpy - function copies the string pointed by
  * src, including the terminating null byte (\0)
  * @dest: function input
  * @src: function input
@@ -13,6 +13,7 @@ char *_strcpy(char *dest, char *src)
 	do {
 		i++;
 		dest[i] = src[i];
-	} while (src[i] != "\0");
+	} while (src[i] != '\0');
+
 	return (dest);
 }
